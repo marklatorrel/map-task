ebff4a7599298f11b8162513315d021675d58b8d
Markyño D. La Torre <marklatorrel@Mac-mini-de-Markyno.local>
2021-04-14 00:33:35 -0500

    version finale

--- mapanimation.js
+++ mapanimation.js
@@ -1,18 +1,14 @@
 // This array contains the coordinates for all bus stops between MIT and Harvard
-const busStops = [
-    [-71.093729, 42.359244],
-    [-71.094915, 42.360175],
-    [-71.0958, 42.360698],
-    [-71.099558, 42.362953],
-    [-71.103476, 42.365248],
-    [-71.106067, 42.366806],
-    [-71.108717, 42.368355],
-    [-71.110799, 42.369192],
-    [-71.113095, 42.370218],
-    [-71.115476, 42.372085],
-    [-71.117585, 42.373016],
-    [-71.118625, 42.374863],
-  ];
+
+  const place = [
+      { 
+          "name": "This is my Home",
+          "coordinates": [-76.968125, -12.104884] },
+
+      { 
+        "name": "Here is were my Job is",
+        "coordinates": [-76.971502, -12.086293] }
+   ]
   
   // TODO: add your own access token
   mapboxgl.accessToken = 'pk.eyJ1IjoibWFya2xhdG9ycmVsIiwiYSI6ImNrbmdweXM3NjAzamkycHAxNWVlcDE1bHAifQ.hQYmIvPQoDOmhXCN_CcjQQ';
@@ -20,27 +16,35 @@ const busStops = [
   // This is the map instance
   let map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-71.104081, 42.365554],
+    style: 'mapbox://styles/mapbox/dark-v10',
+    center: [-76.968125, -12.104884],
     zoom: 14,
   });
+
+  map.addControl(new mapboxgl.NavigationControl());
   
   // TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
   
-  var marker = new mapboxgl.Marker()
-  .setLngLat([-71.093729, 42.359244])
-  .addTo(map);
+  var marker = new mapboxgl.Marker({
+    color: "#FF0000"})
+    .setLngLat([-76.968125, -12.104884])
+    .addTo(map);
+
   
   // counter here represents the index of the current bus stop
   let counter = 0;
+  let poptext = "This is my home";
   function move() {
     // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
     setTimeout(() =>{
-    if(counter>busStops.length) return;
-    marker.setLngLat(busStops[counter]);
+    if(counter>=place.length) counter = 0;
+    poptext = place[counter]["name"];
+   marker.setLngLat(place[counter]["coordinates"])
+    .setPopup(new mapboxgl.Popup().setHTML(poptext))
+    marker.togglePopup();
     counter++;
     move();
-    }, 1000);
+    }, 5000);
    
   }
   
